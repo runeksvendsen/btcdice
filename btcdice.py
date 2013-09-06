ce9b534b720aa929147eedc753ce1414b05d6655
Rune K. Svendsen <runesvend@gmail.com>
2013-09-06 22:12:30 +0200

    Refactor. Add unit tests.

--- btcdice.py
+++ btcdice.py
@@ -20,83 +20,12 @@ Usage:
 	throw enter the result on the screen. Repeat 100 times for 256 bits
 	of entropy."""
 
-class _Getch:
-	"""Gets a single character from standard input.  Does not echo to the
-screen."""
-	def __init__(self):
-		try:
-			self.impl = _GetchWindows()
-		except ImportError:
-			self.impl = _GetchUnix()
-
-	def __call__(self): return self.impl()
-
-
-class _GetchUnix:
-	def __init__(self):
-		import tty, sys
-
-	def __call__(self):
-		import sys, tty, termios
-		fd = sys.stdin.fileno()
-		old_settings = termios.tcgetattr(fd)
-		try:
-			tty.setraw(sys.stdin.fileno())
-			ch = sys.stdin.read(1)
-		finally:
-			termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
-		return ch
-
-
-class _GetchWindows:
-	def __init__(self):
-		import msvcrt
-
-	def __call__(self):
-		import msvcrt
-		return msvcrt.getch()
-
 import sys
-from bitcoin import key as ecdsa
-from bitcoin import base58
-import hashlib
-import Crypto.Hash.SHA256 as sha256
 import argparse
 import math
-
-def decode(s, base):
-	""" Decodes the base<n>-encoded string s into an integer """
-	alphabet = [str(i) for i in range(1,base+1)]
-	decoded = 0
-	multi = 1
-	s = s[::-1]
-	for char in s:
-		decoded += multi * alphabet.index(char)
-		multi = multi * base
-		
-	return decoded
-
-def dsha256(s):
-	return sha256.new(sha256.new(s).digest()).digest()
- 
-def rhash(s):
-	h1 = hashlib.new('ripemd160')
-	h1.update(hashlib.sha256(s).digest())
-	return h1.digest()
-
-def base58_check_encode(s, version=0):
-	vs = chr(version) + s
-	check = dsha256(vs)[:4]
-	return base58.encode(vs + check)
-
-def get_addr(k):
-	pubkey = k.get_pubkey()
-	secret = k.prikey
-	hash160 = rhash(pubkey)
-	addr = base58_check_encode(hash160)
-	payload = secret
-	pkey = base58_check_encode(payload, 128)
-	return addr, pkey
+from getch import _Getch
+import privaddr
+from baseconv import get_alphabet
 
 def getch_print(getch):
 	char = getch()
@@ -113,7 +42,7 @@ def main():
 	entropy_per_throw = math.log(args.faces, 2)
 	THROWS = int(math.ceil(args.entropy / entropy_per_throw))
 
-	alphabet = [str(i) for i in range(1,args.faces+1)]
+	alphabet = get_alphabet(args.faces)
 
 	getch = _Getch()
 
@@ -123,7 +52,7 @@ def main():
 	result = []
 	for i in range(0,THROWS):
 		while True:
-			sys.stdout.write("\nDice throw %d out of %d: " % (THROWS, i+1))
+			sys.stdout.write("\nDice throw %d out of %d: " % (i+1, THROWS))
 			
 			char = getch_print(getch)
 
@@ -150,17 +79,10 @@ def main():
 
 	print "\n"
 
-	num = decode(result, args.faces)
-	secret = hex(num).lstrip('0x').rstrip('L')[0:64].ljust(64, '0').decode('hex')
-
-	key = ecdsa.CKey()
-	key.generate(secret)
-
-	addr,pkey = get_addr(key)
+	addr,pkey = privaddr.throws_to_keyaddr(result, args.faces)
 
 	print "Address: %s" % (addr)
 	print "Private key: %s" % (pkey)
-
 
 if __name__ == "__main__":
 	main()
